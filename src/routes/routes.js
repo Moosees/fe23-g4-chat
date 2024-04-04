f5df55d1df2a727b18b95b85af54ab1ea9831ba7
Sara Johnsson <sara86persson@gmail.com>
2024-04-04 14:06:25 +0200

    added a register and login function

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import ChannelController from "../controller/channelController.js";
 import MessageController from "../controller/messageController.js";
+import UserController from "../controller/userController.js";
 
 const router = Router();
 
@@ -8,5 +9,7 @@ router.put('/channel', ChannelController.createChannel);
 router.post('/channel/:id', MessageController.postMsgToChannel);
 
 router.get('/channel', ChannelController.getAllChannels);
+router.post("/register", UserController.registerUser);
+router.post("/login", UserController.loginUser);
 
 export default router;
