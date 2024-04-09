795b026f5fc29782e736cedb7e643dd4381f4e87
mariagryp <amnotyours@gmail.com>
2024-04-09 12:30:26 +0200

    web + frontend socket.io

--- index.js
+++ index.js
@@ -7,13 +7,53 @@ import { requiresLoggedIn, verifyUser } from './src/middleware/auth.js';
 import chatRoutes from './src/routes/chatRoutes.js';
 import userRoutes from './src/routes/userRoutes.js';
 import publicRoutes from "./src/routes/publicRoutes.js";
-// Used  for create broadcast channel function
-//import ChannelService from "./src/service/channelService.js";
+/* Import Socket.io */
+import { Server } from "socket.io";
 
 // express setup
 const port = 3000;
 const app = express();
 app.use(express.json());
+
+//express server for socket.io
+const expressServer = app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
+
+/**** socket.io ****/
+const io = new Server(expressServer);
+
+io.on('connection', socket => {
+	console.log(`User ${socket.id} connected`);
+
+	//upon connection - only to user
+	socket.emit('message', "Welcome to Chat App!👋");
+
+	//upon connection - to all others
+	socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`);
+
+	//listening for a message event
+	socket.on('message', data => {
+		console.log(data);
+		io.emit('message', `${socket.id.substring(0, 5)} : ${data}`);
+	});
+
+	//listening for dissconect
+	socket.on('disconnect', () => {
+		console.log('user is disconnected');
+		socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`);
+	});
+
+	//listen for activity 
+	socket.on('activity', (name) => {
+		socket.broadcast.emit('activity', name);
+	});
+});
+
+
+// Used  for create broadcast channel function
+//import ChannelService from "./src/service/channelService.js";
+
 
 // database
 mongoose.connect(process.env.MONGODB).then(async () => {
@@ -47,7 +87,9 @@ app.use('/api', userRoutes);
 app.use('/api', verifyUser, publicRoutes);
 app.use('/api', verifyUser, requiresLoggedIn, chatRoutes);
 
+
 // run server
-app.listen(port, () => {
+/* app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
-});
+}); */
+
